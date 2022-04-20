b009e854df221ab1a3a5894cb3e7865b66799c8c
윤민석 <ykmvm147@naver.com>
2022-04-20 21:58:12 +0900

    Make mainpage and design pattern

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33e0b50fbbbd6a3b"; }
+/******/ 		__webpack_require__.h = function() { return "37250b10d3175fbc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ components/templetes/MainPageTemplate.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//Import Components
+import Header from '../header/Header';
+
+const MainPageTemplate = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default MainPageTemplate;
