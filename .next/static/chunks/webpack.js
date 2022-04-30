f78a3626c62e5958c218d724994f8727c95384df
윤민석 <ykmvm147@naver.com>
2022-04-30 15:49:47 +0900

    Postpage styled completegit add .

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b07fd71f48921014"; }
+/******/ 		__webpack_require__.h = function() { return "e3b1858139eab532"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/section/postpage/posts.tsx
+++ components/section/postpage/posts.tsx
@@ -6,15 +6,36 @@ import Link from 'next/link';
 const Posts = () => {
   return (
     <Container>
-      <ContentBox>
-        <Link href="/posts/1">
-          <ThumnailBox>
-            <ThumnailContent>
-              <Thumnail />
-            </ThumnailContent>
-          </ThumnailBox>
-        </Link>
-      </ContentBox>
+      <PostWrap>
+        <PostBox>
+          <Link href="/posts/1">
+            <LinkBox>
+              <ThumnailContent>
+                <Thumnail src={'/image.jpeg'} />
+              </ThumnailContent>
+            </LinkBox>
+          </Link>
+          <ContentBox>
+            <Link href="/posts/1">
+              <LinkBox>
+                <ContentH4>블로그 첫 글</ContentH4>
+                <ContentDescription>
+                  <ContentDescriptionP>
+                    저는 안드로이드 개발을 시작한 지 1년 정도 된 주니어
+                    개발자입니다. 주니어 개발자(3년 이하)의 개발자는 개발 커리어
+                    성장 욕구가 강한 개발자라고 합니다. 그러나 학교나 학원 등
+                    다양한 방법을 통해 이론을 공부하더라고 이를 실제
+                    프로젝트에서 적용할 기회는 흔치 않았습
+                  </ContentDescriptionP>
+                </ContentDescription>
+              </LinkBox>
+            </Link>
+            <SubInfo>
+              <span>2022년 4월 28일</span>
+            </SubInfo>
+          </ContentBox>
+        </PostBox>
+      </PostWrap>
     </Container>
   );
 };
@@ -22,14 +43,17 @@ const Posts = () => {
 export default Posts;
 
 const Container = styled.section`
-  display: flex;
   height: 100vh;
-  padding: 2rem;
-  flex-wrap: wrap;
   background-color: #121212;
 `;
 
-const ContentBox = styled.div`
+const PostWrap = styled.div`
+  display: flex;
+  padding: 2rem;
+  flex-wrap: wrap;
+`;
+
+const PostBox = styled.div`
   width: 20rem;
   background: #1e1e1e;
   border-radius: 4px;
@@ -41,10 +65,11 @@ const ContentBox = styled.div`
   flex-direction: column;
 `;
 
-const ThumnailBox = styled.a`
+const LinkBox = styled.a`
   display: block;
   color: inherit;
   text-decoration: none;
+  cursor: pointer;
 `;
 
 const ThumnailContent = styled.div`
@@ -61,4 +86,47 @@ const Thumnail = styled.img`
   height: 100%;
   display: block;
   object-fit: cover;
+`;
+
+const ContentBox = styled.div`
+  padding: 1rem;
+  display: flex;
+  flex: 1 1 0%;
+  flex-direction: column;
+`;
+
+const ContentH4 = styled.h4`
+  font-size: 1rem;
+  margin: 0px 0px 0.25rem;
+  line-height: 1.5;
+  word-break: break-word;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  color: #ececec;
+`;
+
+const ContentDescription = styled.div`
+  flex: 1 1 0%;
+`;
+
+const ContentDescriptionP = styled.p`
+  margin: 0px 0px 1.5rem;
+  word-break: break-word;
+  overflow-wrap: break-word;
+  font-size: 0.875rem;
+  line-height: 1.5;
+  height: 3.9375rem;
+  display: -webkit-box;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: #d9d9d9;
+`;
+
+const SubInfo = styled.div`
+  font-size: 0.75rem;
+  line-height: 1.5;
+  color: #acacac;
 `;
