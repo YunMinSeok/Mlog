bb7f0b73befcc2a4fea95402879bc04b7df9936d
윤민석 <ykmvm147@naver.com>
2022-07-04 21:52:41 +0900

    [Refactor] post test 코드 리팩토링

--- tests/posts/posts.test.tsx
+++ tests/posts/posts.test.tsx
@@ -3,33 +3,20 @@
 import Posts from '@components/section/postpage/posts';
 import { render, screen } from '@testing-library/react';
 
-describe('<Introduce />', () => {
+describe('<Posts />', () => {
   const setup = () => {
     const utils = render(<Posts />);
-    const firstContent = screen.getByText('This page is MinSeok develop blog.');
-    const secondContent = screen.getByText(
-      'I usally use Typescript, React, Redux.',
-    );
-    const thirdContent = screen.getByText('Thanks for look');
-    const image = screen.getByAltText('Introduce_Logo');
+    const image = screen.getAllByAltText('post_image');
 
     return {
       utils,
-      firstContent,
-      secondContent,
-      thirdContent,
       image,
     };
   };
   it('has content and a image', () => {
-    const { firstContent, secondContent, thirdContent, image } = setup();
-    expect(firstContent).toBeInTheDocument();
-    expect(secondContent).toBeInTheDocument();
-    expect(thirdContent).toBeInTheDocument();
+    const { image } = setup();
     expect(image).toBeInTheDocument();
   });
 
-  it('uses correct src', async () => {
-    const { image } = setup();
-  });
+  it('uses correct src', async () => {});
 });
