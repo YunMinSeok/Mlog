37b5d53182dee2fb24764f6798b50e671a05482d
윤민석 <ykmvm147@naver.com>
2022-08-19 22:57:01 +0900

    [Feat] slider image url change

--- pages/posts/slider/index.tsx
+++ pages/posts/slider/index.tsx
@@ -6,11 +6,11 @@ import Slider from '../../../components/section/postpage/slider/SliderPage';
 
 const Index = () => {
   const images = [
-    { pic: '/Mlog/SliderImage1.jpeg', id: 1 },
-    { pic: '/Mlog/SliderImage2.jpeg', id: 2 },
-    { pic: '/Mlog/SliderImage3.jpeg', id: 3 },
-    { pic: '/Mlog/SliderImage4.jpeg', id: 4 },
-    { pic: '/Mlog/SliderImage5.jpeg', id: 5 },
+    { pic: '/SliderImage1.jpeg', id: 1 },
+    { pic: '/SliderImage2.jpeg', id: 2 },
+    { pic: '/SliderImage3.jpeg', id: 3 },
+    { pic: '/SliderImage4.jpeg', id: 4 },
+    { pic: '/SliderImage5.jpeg', id: 5 },
   ];
 
   return (
