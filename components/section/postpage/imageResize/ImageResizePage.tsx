0ffef8bafedc95fbf2ba76440d61d4c76d3434a5
윤민석 <ykmvm147@naver.com>
2022-08-31 20:30:49 +0900

    [Feat] Add  ImageResize Page backbutton

--- components/section/postpage/imageResize/ImageResizePage.tsx
+++ components/section/postpage/imageResize/ImageResizePage.tsx
@@ -1,5 +1,29 @@
+import { useRouter } from 'next/router';
+import React from 'react';
+
+//styled
+import * as Styled from './ImageResizeStyle';
+
+import { ARROW_IMAGE } from '../../../../constants/image/image';
+
 const ImageResizePage = () => {
-  return <></>;
+  const loaderProp = ({ src }: { src: string }) => {
+    return src;
+  };
+
+  const router = useRouter();
+  return (
+    <>
+      <Styled.BackButton
+        onClick={() => router.back()}
+        loader={loaderProp}
+        src={ARROW_IMAGE}
+        alt={'arrow'}
+        width={50}
+        height={50}
+      />
+    </>
+  );
 };
 
 export default ImageResizePage;
--- components/section/postpage/imageResize/ImageResizeStyle.ts
+++ components/section/postpage/imageResize/ImageResizeStyle.ts
@@ -1,1 +1,8 @@
+import Image from 'next/image';
 import styled from 'styled-components';
+
+export const BackButton = styled(Image)`
+  transform: rotate(180deg);
+  cursor: pointer;
+  -webkit-filter: opacity(0.5) drop-shadow(0 0 0 #fff);
+`;
