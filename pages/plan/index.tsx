7b29f55273d6d253101acd386aab1e4b48d70e0b
윤민석 <ykmvm147@naver.com>
2022-09-20 22:02:14 +0900

    feat: Add Plan page content

--- pages/plan/index.tsx
+++ pages/plan/index.tsx
@@ -1,8 +1,7 @@
 //components
-import PostPage from '../../components/templetes/PostsPageTemplate';
 
 const Post = () => {
-  return <>{/* <PostPage /> */}</>;
+  return <div>준비 중입니다.</div>;
 };
 
 export default Post;
