8078d07b104b1b2165582eabd2bdf8a2e7a654ba
윤민석 <ykmvm147@naver.com>
2022-09-21 22:01:43 +0900

    feat: Update get user data with redux thunk

--- modules/slice/user.ts
+++ modules/slice/user.ts
@@ -11,24 +11,49 @@ export const loadUser = createAsyncThunk(``, async (id, thunkApi) => {
     //or return thunkApi.rejectWithValue(err);
   }
 });
-
-export interface User {
+interface User {
   id: number;
-  name: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  avatar: string;
 }
+
+interface UserState {
+  user: User | null;
+  loadUserLoading: boolean;
+  loadUserDone: boolean;
+  loadUserError: any;
+}
+
+const initialState: UserState = {
+  user: null,
+  loadUserLoading: false,
+  loadUserDone: false,
+  loadUserError: null,
+};
 
 export const userSlice = createSlice({
   name: 'users',
-  initialState: [{ id: 1, name: '은지' }] as User[],
-  reducers: {
-    addUser: (state, action: PayloadAction<User>) => {
-      const { id, name } = action.payload;
-      state.push({ id, name });
-    },
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(loadUser.pending, (state) => {
+        state.loadUserLoading = true;
+      })
+      .addCase(loadUser.fulfilled, (state, action) => {
+        state.loadUserDone = true;
+        state.user = action.payload;
+      })
+      .addCase(loadUser.rejected, (state, action) => {
+        state.loadUserLoading = false;
+        state.loadUserDone = true;
+        state.loadUserError = action.payload;
+      });
   },
 });
 
 const { actions, reducer } = userSlice;
-export const { addUser } = actions;
 
 export default reducer;
