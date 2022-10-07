d4d5cb3711553ced921c248bb8e20d0c0237034b
윤민석 <ykmvm147@naver.com>
2022-10-07 21:47:08 +0900

    feat: Update setting header content test

--- tests/header/header.test.tsx
+++ tests/header/header.test.tsx
@@ -6,8 +6,20 @@ import { render, screen } from '@testing-library/react';
 describe('<Introduce />', () => {
   const setup = () => {
     const utils = render(<Header />);
+    const firstLink = screen.getByText('Mlog');
+    const secondLink = screen.getByText('Introduce');
+    const thirdLink = screen.getByText('Posts');
+    const fourthLink = screen.getByText('Plan');
 
-    return {};
+    return {
+      utils,
+      firstLink,
+      secondLink,
+      thirdLink,
+      fourthLink,
+    };
   };
-  it('has content and a image', () => {});
+  it('is correct link content', () => {
+    const { firstLink, secondLink, thirdLink, fourthLink } = setup();
+  });
 });
