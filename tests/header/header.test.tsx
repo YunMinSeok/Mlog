4a566ce0bfa24cdfefcfdad503e6363faad51d5a
윤민석 <ykmvm147@naver.com>
2022-10-07 21:36:29 +0900

    feat: Create header test file

---
+++ tests/header/header.test.tsx
@@ -0,0 +1,13 @@
+//header test
+
+import Header from '@components/header/Header';
+import { render, screen } from '@testing-library/react';
+
+describe('<Introduce />', () => {
+  const setup = () => {
+    const utils = render(<Header />);
+
+    return {};
+  };
+  it('has content and a image', () => {});
+});
