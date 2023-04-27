d990692815f917ec51a975969171c1d5ffe3ea1d
윤민석 <ykmvm147@naver.com>
2023-04-27 22:01:26 +0900

    feat: 타이머 페이지 렌더링 테스트, 요소확인 테스트 진행 완료

--- tests/posts/timer.test.tsx
+++ tests/posts/timer.test.tsx
@@ -1,3 +1,18 @@
-import React from 'react';
+import { render, screen, cleanup } from '@testing-library/react';
 
-describe('타이머 페이지 테스트', () => {});
+// components
+import TimerPage from '../../components/section/postpage/timer/TimerPage';
+
+describe('타이머 페이지 테스트', () => {
+  // 1. 렌더링 테스트
+  beforeEach(() => {
+    render(<TimerPage />);
+  });
+
+  afterEach(cleanup);
+
+  // 2. 요소 확인 테스트
+  it('로그인 페이지 렌더링 테스트', async () => {
+    expect(screen.getByText('타이머 시작하기')).toBeInTheDocument;
+  });
+});
