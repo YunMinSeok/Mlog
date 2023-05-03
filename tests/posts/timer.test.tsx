056b7a4146f577a06fb85ac52e42b88124099a79
윤민석 <ykmvm147@naver.com>
2023-05-03 21:24:27 +0900

    feat: hook 테스트를 위한 테스트로직 및 변수 선언

--- tests/posts/timer.test.tsx
+++ tests/posts/timer.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { renderHook, act } from '@testing-library/react-hooks';
+import { useState } from 'react';
 
 // components
 import TimerPage from '../../components/section/postpage/timer/TimerPage';
 
+// hook
+import { UseTimer } from '@/src/hook/useTimer';
+
 describe('타이머 페이지 테스트', () => {
+  const [timer, setTimer] = useState(60);
+  const [isStart, setIsTimeStart] = useState(false);
+  const [isTimeOver, setIsTimeOver] = useState(false);
   // 1. 렌더링 테스트
   beforeEach(() => {
     render(<TimerPage />);
@@ -27,9 +35,17 @@ describe('타이머 페이지 테스트', () => {
     // 타이머 시작하기
     await userEvent.click(timerButton);
 
+    // 일정 시간 지난 후 테스트
     await new Promise((r) => setTimeout(r, 3000));
 
     expect(timerButton).toHaveTextContent('타이머 멈추기');
     expect(screen.getByText('0:01')).toBeInTheDocument();
+  });
+
+  // 4. 훅 return값 테스트
+  it('타이머 버튼 테스트', async () => {
+    const { result } = renderHook(() =>
+      UseTimer(timer, setTimer, isStart, setIsTimeStart, setIsTimeOver),
+    );
   });
 });
